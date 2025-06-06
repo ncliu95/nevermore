6affaefee00c0d7059888aba3f5a1659ac8039b9
WilliamKoglin <207467986+WilliamKoglin@users.noreply.github.com>
2025-06-06 13:57:20 -0500

    stuff no longer missing. time to style

--- app/src/pages/will.js
+++ app/src/pages/will.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import '../App.css';
 
 function Will() {
-  const [inputValue, setInputValue ] = useState('');
+    const [inputValue, setInputValue ] = useState('');
     const [displayText, setDisplayText] = useState('most people');
   
     const [basketballInput, setBasketballInput] = useState('');
@@ -26,77 +26,77 @@ function Will() {
 
     return (
         <>
-    <div className="row">
-            <img src={"/assets/goat.jpeg"} className="App-bron" alt="Lebron James" />
-            <p>
-              Lebron James is&nbsp;
-              <span
-              className="yellow"
-              style={{ color: '#ffff00' }}>
-                faster&nbsp;
-              </span>
-              <span id="Letext">than {displayText}</span> 
-            </p>
-            <input
-              type="text"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              placeholder="Who is the fastest person you know?"
-              size ="34"
-            />
-            <a
-              className="App-link-will"
-              href="https://www.espn.com/nba/player/_/id/1966/lebron-james"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: '#ffffff' }}
-            >
-              More about LeBron James
-            </a>
-          </div>
-          
-<div className="row">
-<div className="box-wrapper">
-    <div className="outerBox">
-    {messages.map((msg, index) => (
-        <div key={index} className="chat-message">
-        {msg}
-        </div>
-    ))}
-    </div>
-    <div className="innerBox">
-    <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-        <label htmlFor="greatestPlayerInput" style={{ fontSize: '12px', color: '#222' }}>
-        Who is the greatest basketball player?
-        </label>
-        <div className="input-group">
-        <input
-            id="greatestPlayerInput"
-            type="text"
-            value={basketballInput}
-            onChange={(e) => setBasketballInput(e.target.value)}
-            placeholder="Type a name..."
-            className="chat-input"
-            onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-                handleSend();
-            }
-            }}
-        />
-        <button
-            onClick={handleSend}
-            className="send-button"
-        >
-            Send
-        </button>
-        </div>
-    </div>
-    </div>
+        <div className="container">
+            <div className="row">
+                    <img src={"/assets/goat.jpeg"} className="App-bron" alt="Lebron James" />
+                    <p>
+                    Lebron James is&nbsp;
+                    <span
+                    className="yellow"
+                    style={{ color: '#ffff00' }}>
+                        faster&nbsp;
+                    </span>
+                    <span id="Letext">than {displayText}</span> 
+                    </p>
+                    <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Who is the fastest person you know?"
+                    size ="34"
+                    />
+                    <a
+                    className="App-link-will"
+                    href="https://www.espn.com/nba/player/_/id/1966/lebron-james"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ color: '#ffffff' }}
+                    >
+                    More about LeBron James
+                    </a>
             </div>
-          </div>
-          
-
-</>
+            
+            <div className="row">
+                <div className="box-wrapper">
+                    <div className="outerBox">
+                    {messages.map((msg, index) => (
+                        <div key={index} className="chat-message">
+                        {msg}
+                        </div>
+                    ))}
+                    </div>
+                    <div className="innerBox">
+                    <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+                        <label htmlFor="greatestPlayerInput" style={{ fontSize: '12px', color: '#222' }}>
+                        Who is the greatest basketball player?
+                        </label>
+                        <div className="input-group">
+                        <input
+                            id="greatestPlayerInput"
+                            type="text"
+                            value={basketballInput}
+                            onChange={(e) => setBasketballInput(e.target.value)}
+                            placeholder="Type a name..."
+                            className="chat-input"
+                            onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                handleSend();
+                            }
+                            }}
+                        />
+                        <button
+                            onClick={handleSend}
+                            className="send-button"
+                        >
+                            Send
+                        </button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
           
   );
 }
