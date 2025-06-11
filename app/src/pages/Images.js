f066549b19e9e38a74b8a1cf1ae4c57d0766170f
Teatoes <benwang1515@gmail.com>
2025-06-11 16:04:52 -0500

    organized css files

--- app/src/pages/Images.js
+++ app/src/pages/Images.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import '../App.css';
+import '../cssFiles/Images.css';
 
 function Images() {
       const [animal, setAnimal] = useState("cats");
