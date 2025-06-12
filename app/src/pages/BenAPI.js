ea89a0c64d239d865379ed576d88c19b460b7a78
Teatoes <benwang1515@gmail.com>
2025-06-11 22:19:34 -0500

    added post feature

--- app/src/pages/BenAPI.js
+++ app/src/pages/BenAPI.js
@@ -3,18 +3,54 @@ import '../cssFiles/BenAPI.css'
 
 function BenAPI(){
 
+  const [hex, setHex] = useState('');
+  const [colorName, setColorName] = useState('');
   const [colors, setColors] = useState([]);
 
-  useEffect(() => {
-      fetch('https://api.sampleapis.com/csscolornames/colors')
-      .then(response => response.json())
-      .then(data => setColors(data))
-      .catch(error => console.error('Error fetching colors:', error));
-  }, []);
+  useEffect(() => { fetchColor();}, []);
+  const fetchColor = async() => {
+      const res = await fetch('https://api.sampleapis.com/csscolornames/colors')
+      const data = await res.json();
+      setColors(data);
+  };
+
+  const handlePost = async () => {
+  const res = await fetch('https://api.sampleapis.com/csscolornames/colors', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name: colorName, hex: hex })
+  });
+  if (res.ok){
+    setHex('');
+    setColorName('');
+    fetchColor();
+  }
+
+};
 
   return (
     <div className="main">
       <h1 className="header">CSS Colors</h1>
+
+       <div>
+      <input
+        type="text"
+        value={colorName}
+        onChange={(e) => setColorName(e.target.value)}
+        placeholder='Black'
+      />
+      <input
+        type="text"
+        value={hex}
+        onChange={(e) => setHex(e.target.value)}
+        placeholder='#000000'
+      />
+      <button onClick={handlePost}>Add</button>
+    </div>
+
+
         <div className="container">
       {colors.map((color, index) => (
         <div
