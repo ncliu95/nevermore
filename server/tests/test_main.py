5a250a8e161e00e4b57737bdd4c80ca9a9055292
WilliamKoglin <207467986+WilliamKoglin@users.noreply.github.com>
2025-06-23 20:33:53 -0500

    post function unit test

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -8,3 +8,13 @@ def test_read_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the Nevermore API"}
 
+def test_add():
+    response = client.post("/will/post", json={"typeJoke": "general", "setup": "test setup", "punchline": "test punchline"})
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "id" in data
+    assert isinstance(data["id"], str)
+    assert data["type"]== "general"
+    assert data["setup"]== "test setup"
+    assert data["punchline"] == "test punchline"
