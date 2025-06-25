485c5c8a64430c1b59ef388256c8f8f9bd723245
WilliamKoglin <207467986+WilliamKoglin@users.noreply.github.com>
2025-06-25 12:47:33 -0500

    Revert "made the navbar more scalable (might not be the right word) and moved the homepage from /home to / so that it pops up by default"
    This reverts commit 052759391a6bfe101c19c77ea2542f0a9e267004.

--- app/src/components/NavBar/index.js
+++ app/src/components/NavBar/index.js
@@ -7,61 +7,76 @@ const Navbar = () => {
 
   const navigate = useNavigate();
 
-  const handleNav = (path, title) => {
-  navigate(path);
-  setPage(title);
-};
+  const homenav = () => {
+    navigate("/home");
+    setPage("Home");
+  };
+  const bennav = () => {
+    navigate("/ben");
+    setPage("Ben's Page");
+  };
+  const willnav = () => {
+    navigate("/will");
+    setPage("Will's Page");
+  };
+  const benimagenav = () => {
+    navigate("/BenImages");
+    setPage("Ben's Images");
+  };
 
-const benLinks = [
-  { label: "Ben's Page", path: "/ben" },
-  { label: "Images", path: "/BenImages" },
-  { label: "Ben's API", path: "/BenAPI" },
-];
+  const willimagenav = () => {
+    navigate("/WillImages");
+    setPage("Will's Images");
+  };
 
-const willLinks = [
-  { label: "Will's Page", path: "/will" },
-  { label: "Images", path: "/WillImages" },
-  { label: "Will's API", path: "/WillApi" },
-];
+  const ben_apinav = () => {
+    navigate("/BenAPI");
+    setPage("Ben's API");
+  };
 
-return (
-  <div className="navBar">
-    <button className="home-button" onClick={() => handleNav("/", "Home")}>Home</button>
+  const will_apinav = () => {
+    navigate("/WillApi");
+    setPage("Will's API");
+  };
+  return (
+    <div className="navBar">
+      <button className="home-button" onClick={homenav}>
+        Home
+      </button>
 
-    <div className="dropdown">
-      <button className="dropdown-button">Ben ▾</button>
-      <div className="dropdown-content">
-        {benLinks.map(link => (
-          <div
-            key={link.path}
-            onClick={() => handleNav(link.path, link.label)}
-            className="dropdown-options"
-          >
-            {link.label}
+      <div className="dropdown">
+        <button className="dropdown-button">Ben ▾</button>
+        <div className="dropdown-content">
+          <div onClick={bennav} className="dropdown-options">
+            Ben's Page
           </div>
-        ))}
-      </div>
-    </div>
-
-    <div className="dropdown">
-      <button className="dropdown-button">Will ▾</button>
-      <div className="dropdown-content">
-        {willLinks.map(link => (
-          <div
-            key={link.path}
-            onClick={() => handleNav(link.path, link.label)}
-            className="dropdown-options"
-          >
-            {link.label}
+          <div onClick={benimagenav} className="dropdown-options">
+            Images
           </div>
-        ))}
+          <div onClick={ben_apinav} className="dropdown-options">
+            Ben's API
+          </div>
+        </div>
       </div>
+
+      <div className="dropdown">
+        <button className="dropdown-button">Will ▾</button>
+        <div className="dropdown-content">
+          <div onClick={willnav} className="dropdown-options">
+            Will's Page
+          </div>
+          <div onClick={willimagenav} className="dropdown-options">
+            Images
+          </div>
+          <div onClick={will_apinav} className="dropdown-options">
+            Will's API
+          </div>
+        </div>
+      </div>
+
+      <div className="page-title">{page}</div>
     </div>
-
-    <div className="page-title">{page}</div>
-  </div>
-);
-
+  );
 };
 
 export default Navbar;
