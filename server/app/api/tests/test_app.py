fee27b5f4164a009f70ed9b7fabf192dcca2692a
WilliamKoglin <207467986+WilliamKoglin@users.noreply.github.com>
2025-06-27 21:19:24 -0500

    test cases

--- server/app/api/tests/test_app.py
+++ server/app/api/tests/test_app.py
@@ -12,14 +12,4 @@ def test_default():
     assert response.json() == {"message": "Welcome to the Nevermore API"}
 
 
-def test_add_joke():
-    response = client.post("/will/post", json={"typeJoke": "general", "setup": "test setup", "punchline": "test punchline"})
-    assert response.status_code == 200
-    data = response.json()
-
-    assert "id" in data
-    assert isinstance(data["id"], str)
-    assert data["type"]== "general"
-    assert data["setup"]== "test setup"
-    assert data["punchline"] == "test punchline"
     
