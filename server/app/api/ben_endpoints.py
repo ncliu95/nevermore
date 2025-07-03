494cb3d8ed38085730538c86c552360bb22b9630
WilliamKoglin <207467986+WilliamKoglin@users.noreply.github.com>
2025-07-02 19:07:31 -0500

    ben's bot and resume is good to go

--- server/app/api/ben_endpoints.py
+++ server/app/api/ben_endpoints.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.models.request_models import PromptRequest, PromptResponse
 from fastapi import HTTPException
 from app.services.openai_service import get_openai_response
-from app.prompts.prompts import BEN_PROMPT, WILL_RESUME
+from app.prompts.prompts import BEN_PROMPT, BEN_RESUME
 
 router = APIRouter()
 
@@ -15,7 +15,7 @@ def get_hello_world():
 
 
 @router.post("/generate", response_model=PromptResponse)
-async def generate_response(request: PromptRequest, system_prompt = BEN_PROMPT, system_resume = WILL_RESUME):
+async def generate_response(request: PromptRequest, system_prompt = BEN_PROMPT, system_resume = BEN_RESUME):
     try:
         response_text = get_openai_response(request.prompt, system_prompt,system_resume)
         return PromptResponse(response=response_text)
